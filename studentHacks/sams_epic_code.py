586be6f414d6fda555f51165a16d26a45cac01f8
Shaun Miller <miller.shaun.s@scsc.vic.edu.au>
2019-12-05 21:48:40 +0000

    Student Update

--- studentHacks/sams_epic_code.py
+++ studentHacks/sams_epic_code.py
@@ -2,9 +2,9 @@ import datetime
 while 1:  
   try:
     #checks to see if your input can be coverted to an int
-    dob = int(input('What year where you born in? '))
+    DOB = int(input('What year where you born in? '))
     break
   except:
     print('invalid input\n')
 date = datetime.datetime.today()
-print('You are',date.year - dob,'years old.\n')
+print('You are',date.year - DOB,'years old.\n')
